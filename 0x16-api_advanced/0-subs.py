e4a8fb8b6d4ea80f16a44a59a51fe6642217bdea
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-01-17 02:35:35 +0200

    API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Queries Reddit API and returns the number of total subscribers for a given
+subreddit
+"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries Reddit API and returns the number of total subscribers for a
+    given subreddit
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = get(url, headers=headers)
+    if (not response.ok):
+        return 0
+    total = response.json().get('data').get('subscribers')
+    return total
