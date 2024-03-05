9e5f7159419cc332015516bdf0c3c3e7798611c6
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-03-05 18:18:33 +0200

    num of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""queries Reddit API for number of subscribers on given subreddit."""
+"""
+queries the Reddit API and returns the number of subscribers for a
+given subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the total number of subscribers on a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0\
-        Safari/537.36"
+        Safari/537.36'
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
