a10e4a356a60b0b4874d036e2a339febe2da8d3b
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-03-05 16:36:23 +0200

    0x16. API advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""queries reddit API for the first 10 host posts on a subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the 10 hottest posts on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /thapelo)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,8 @@
+# 0x16. API advanced
+
+- Facilitator: Tim Britton
+- Author: Thapelo M Moumakoe
+- Language: `Python`
+- Subjects: `Scripting`, `Back-end`, `API`
+
+![](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-sysadmin_devops/314/WIxXad8.png)
