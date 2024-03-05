86af3a02f0506d62e299c22e28c7f74bbf60adf1
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-03-05 16:58:17 +0200

    User-Agent fix 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,9 @@ def number_of_subscribers(subreddit):
     """returns the total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "ubuntu:0x16.api.advanced:v1.0.0 (by thapelocodes)"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0\
+        Safari/537.36"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,9 @@ def top_ten(subreddit):
     """prints the titles of the 10 hottest posts on a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "ubuntu:0x16.api.advanced:v1.0.0 (by /thapelocodes)"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0\
+        Safari/537.36"
     }
     params = {
         "limit": 10
